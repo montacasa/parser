f713297d46a41620a6cf21152f92433e40422717
Gustavo Tonietto <tonietto@gmail.com>
2018-05-28 10:11:42 -0300

    [Tests] Add type tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,4 @@
+/* flow */
 /* global test expect */
 import Parser from './';
 
@@ -10,10 +11,28 @@ test('should parse a float', () => {
   expect(Parser.parseFloat('1.2')).toBe(1.2);
 });
 
+test('should parse an empty param', () => {
+  expect(Parser.isEmptyParam('')).toBe(true);
+});
+
+test('should parse an integer', () => {
+  // NOTE: Should it realy return only true or false?
+  expect(Parser.isEmptyParam(1)).toBe(true);
+  expect(Parser.isEmptyParam('1')).toBe(false);
+});
+
+test('should parse empty field', () => {
+  expect(Parser.isNotEmpty('1')).toBe(true);
+  // NOTE: Should it realy return the string in case is not empty?
+  expect(Parser.isNotEmpty('')).toBe('');
+});
+
 test('should slugify', () => {
   expect(Parser.slugify('A String')).toBe('astring');
 });
 
 test('should slugify with override rule', () => {
-  expect(Parser.slugify('A String', {lower: true, remove: null, replacement: '-'})).toBe('a-string');
+  expect(
+    Parser.slugify('A String', {lower: true, remove: null, replacement: '-'}),
+  ).toBe('a-string');
 });
